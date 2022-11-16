750003526f1df542049069eaf3408157e1139bd6
patrie <pmespert@gmail.com>
2022-11-16 16:26:12 +0100

    improving game result on api call

--- app/src/main/java/com/patriciamespert/mygamesac/GameResult.kt
+++ app/src/main/java/com/patriciamespert/mygamesac/GameResult.kt
@@ -1,50 +1,60 @@
 package com.patriciamespert.mygamesac
 
 data class GameResult(
-    val added: Int,
-    val added_by_status: AddedByStatus,
-    val background_image: String,
-    val esrb_rating: EsrbRating,
     val id: Int,
-    val metacritic: Int,
-    val name: String,
-    val platforms: List<Platform>,
-    val playtime: Int,
-    val rating: Int,
-    val rating_top: Int,
-    val ratings: Ratings,
-    val ratings_count: Int,
-    val released: String,
-    val reviews_text_count: String,
     val slug: String,
-    val suggestions_count: Int,
+    val name: String,
+    val released: String,
     val tba: Boolean,
-    val updated: String
+    val backgroud_image:String,
+    val rating: Double,
+    val rating_top: Int,
+    val ratings: List<Rating>,
+    val ratings_count: Int,
+    val reviews_text_count: Int,
+    val added: Int,
+    val added_by_status:StatusObj,
+    val metacritic: Int,
+    val playtime: Int,
+    val suggestions_count:Int,
+    val esbr_rating:EsbrRatingObj,
+    val platforms:List<PlatformObj>
+
 )
 
-class AddedByStatus
-
-data class EsrbRating(
+data class Rating(
     val id: Int,
-    val name: String,
-    val slug: String
+    val title: String,
+    val count: Int,
+    val percent: Double
+)
+
+data class StatusObj(
+    val owned: Int,
+    val beaten: Int,
+    val toplay: Int,
+    val playing: Int
+)
+
+data class EsbrRatingObj(
+    val id: Int,
+    val slug: String,
+    val name: String
+)
+
+data class PlatformObj(
+    val platform:Platform,
+    val released: String,
+    val requirements:RequirementObj
 )
 
 data class Platform(
-    val platform: PlatformX,
-    val released_at: String,
-    val requirements: Requirements
-)
-
-class Ratings
-
-data class PlatformX(
     val id: Int,
-    val name: String,
-    val slug: String
+    val slug: String,
+    val name: String
 )
 
-data class Requirements(
+data class RequirementObj(
     val minimum: String,
     val recommended: String
 )
