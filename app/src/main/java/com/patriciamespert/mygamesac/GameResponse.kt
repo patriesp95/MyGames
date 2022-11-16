0c110eb6309bfaa44aa608f5a0428dbc5113ee31
patrie <pmespert@gmail.com>
2022-11-16 16:25:25 +0100

    improving server's response

--- app/src/main/java/com/patriciamespert/mygamesac/GameResponse.kt
+++ app/src/main/java/com/patriciamespert/mygamesac/GameResponse.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class GameResponse(
     @SerializedName("count") var totalGames: Int,
-    @SerializedName("next") var nextPage: Int,
-    @SerializedName("previous")  var prevPage:Int,
+    @SerializedName("next") var nextPage: String,
+    @SerializedName("previous")  var prevPage:String,
     @SerializedName("results")  var games: List<GameResult>
 )
