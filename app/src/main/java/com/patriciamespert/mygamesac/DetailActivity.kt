28520a186256147737c1865bcc7f49c65637a2a5
patrie <pmespert@gmail.com>
2022-11-17 12:03:59 +0100

    Showing game description in DetailActivity

--- app/src/main/java/com/patriciamespert/mygamesac/DetailActivity.kt
+++ app/src/main/java/com/patriciamespert/mygamesac/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.patriciamespert.mygamesac
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import com.bumptech.glide.Glide
 import com.patriciamespert.mygamesac.databinding.ActivityDetailBinding
 
@@ -17,10 +19,15 @@ class DetailActivity : AppCompatActivity() {
 
         val game = intent.getParcelableExtra<GameDetailResponse>(EXTRA_GAME)
         if (game != null){
-            /*title = game.name*/
+            title = game.gameName
             Glide.with(this)
                 .load(game.gameBackgroundImage)
                 .into(binding.backdrop)
+            binding.description.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Html.fromHtml(game.gameDescription, Html.FROM_HTML_MODE_COMPACT)
+            } else {
+                Html.fromHtml(game.gameDescription)
+            }
 
         }
     }
