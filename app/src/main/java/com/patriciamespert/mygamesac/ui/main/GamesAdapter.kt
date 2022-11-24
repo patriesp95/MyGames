1786ec88737b2ae5591d068ea50f3eb8357bfd04
patrie <pmespert@gmail.com>
2022-11-24 11:10:37 +0100

    Revert "Revert "Initial commit, MVVM implemented""
    This reverts commit 1cfd3c364031a67fa56bc724de369b2924d208d4.

--- app/src/main/java/com/patriciamespert/mygamesac/ui/main/GamesAdapter.kt
+++ app/src/main/java/com/patriciamespert/mygamesac/ui/main/GamesAdapter.kt
@@ -1,4 +1,4 @@
-package com.patriciamespert.mygamesac.ui
+package com.patriciamespert.mygamesac.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
---
+++ app/src/main/java/com/patriciamespert/mygamesac/ui/main/MainViewModel.kt
@@ -0,0 +1,67 @@
+package com.patriciamespert.mygamesac.ui.main
+
+import androidx.lifecycle.*
+import com.patriciamespert.mygamesac.GameDetailResponse
+import com.patriciamespert.mygamesac.GameResult
+import com.patriciamespert.mygamesac.model.GamesRepository
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel(private val gamesRepository: GamesRepository): ViewModel() {
+
+    private val _state = MutableLiveData(UiState())
+    val state: LiveData<UiState> get() {
+        if(_state.value?.games == null){
+            refresh()
+        }
+        return _state
+    }
+
+    private fun refresh() {
+        viewModelScope.launch {
+            _state.value = UiState(loading = true)
+            _state.value = UiState(games = gamesRepository.findPopularGames().body()?.games)
+        }
+    }
+
+    fun onGameClicked(game: GameResult) =
+        performGameInformationRetrieval(gamesRepository.findGameDetails(game))
+
+
+    private fun performGameInformationRetrieval(gameCall: Call<GameDetailResponse>) {
+        gameCall.enqueue(object : Callback<GameDetailResponse> {
+            override fun onResponse(
+                call: Call<GameDetailResponse>,
+                response: Response<GameDetailResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val game = response.body()
+                    game?.let {
+                        _state.value = _state.value?.copy(navigateTo = game )
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<GameDetailResponse>, t: Throwable) {
+                _state.value = _state.value?.copy(navigateTo = null)
+            }
+
+        })
+    }
+
+    data class UiState(
+        var loading: Boolean = false,
+        var games: List<GameResult>? = null,
+        val navigateTo: GameDetailResponse? = null
+    )
+}
+
+@Suppress("UNCHECKED_CAST")
+class MainViewModelFactory(private val gamesRepository: GamesRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(gamesRepository) as T
+    }
+}
