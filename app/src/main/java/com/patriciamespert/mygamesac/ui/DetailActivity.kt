1cfd3c364031a67fa56bc724de369b2924d208d4
patrie <pmespert@gmail.com>
2022-11-24 11:02:12 +0100

    Revert "Initial commit, MVVM implemented"
    This reverts commit 31072ae980d585eb306fbae3348e9883d4770499.

---
+++ app/src/main/java/com/patriciamespert/mygamesac/ui/DetailActivity.kt
@@ -0,0 +1,56 @@
+package com.patriciamespert.mygamesac.ui
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import com.bumptech.glide.Glide
+import com.patriciamespert.mygamesac.GameDetailResponse
+import com.patriciamespert.mygamesac.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_GAME = "DetailActivity:game"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val game = intent.getParcelableExtra<GameDetailResponse>(EXTRA_GAME)
+        if (game != null){
+            title = game.gameName
+            Glide.with(this)
+                .load(game.gameBackgroundImage)
+                .into(binding.backdrop)
+            binding.description.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Html.fromHtml(game.gameDescription, Html.FROM_HTML_MODE_COMPACT)
+            } else {
+                Html.fromHtml(game.gameDescription)
+            }
+            bindDetailInfo(binding.detailGameInfo, game)
+
+        }
+    }
+
+    private fun bindDetailInfo(detailInfo: TextView, game: GameDetailResponse) {
+        detailInfo.text = buildSpannedString {
+
+            bold { append("Original title: ") }
+            appendLine(game.gameNameOriginal)
+
+            bold { append("Release date: ") }
+            appendLine(game.gameReleasedDate)
+
+            bold { append("Rating: ") }
+            appendLine(game.gameRating.toString())
+
+            bold { append("Top Rating: ") }
+            appendLine(game.gameRatingTop.toString())
+        }
+    }
+}
--- app/src/main/java/com/patriciamespert/mygamesac/ui/GamesAdapter.kt
+++ app/src/main/java/com/patriciamespert/mygamesac/ui/GamesAdapter.kt
@@ -1,4 +1,4 @@
-package com.patriciamespert.mygamesac.ui.main
+package com.patriciamespert.mygamesac.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
