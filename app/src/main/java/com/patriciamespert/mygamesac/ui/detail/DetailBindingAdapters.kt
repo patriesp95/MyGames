d14c333b2d9296dc7b3deb191d98378d5ea53e98
patriesp95 <patrimespert@outlook.com>
2022-11-26 18:41:50 +0100

    Added some detail binding adapters

--- app/src/main/java/com/patriciamespert/mygamesac/ui/detail/DetailBindingAdapters.kt
+++ app/src/main/java/com/patriciamespert/mygamesac/ui/detail/DetailBindingAdapters.kt
@@ -1,11 +1,23 @@
 package com.patriciamespert.mygamesac.ui.detail
 
+import android.os.Build
+import android.text.Html
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.patriciamespert.mygamesac.model.database.detail.GameDetail
 
 @BindingAdapter("game")
-fun GameDetailInfoView.updateMovieDetails(game: GameDetail?) {
+fun GameDetailInfoView.updateGameDetails(game: GameDetail?) {
     if (game != null) {
         setGame(game)
+    }
+}
+
+@BindingAdapter("description")
+fun TextView.toHtml(description: String ){
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(description, Html.FROM_HTML_MODE_COMPACT)
+    } else {
+        Html.fromHtml(description)
     }
 }
