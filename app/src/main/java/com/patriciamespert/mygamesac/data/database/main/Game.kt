43ae3d8512a13a462de796d14c71fb4a0e01f8c8
patriesp95 <patrimespert@outlook.com>
2022-11-27 18:34:52 +0100

    adapt models

---
+++ app/src/main/java/com/patriciamespert/mygamesac/data/database/main/Game.kt
@@ -0,0 +1,18 @@
+package com.patriciamespert.mygamesac.data.database.main
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity
+data class Game (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    val released: String,
+    val backgroundImage:String,
+    val rating: Double,
+    val ratingTop: Int,
+    val added: Int
+) : Parcelable
